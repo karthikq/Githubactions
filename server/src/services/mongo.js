4de8e1928e945ae789ceea1bb38ae234e5a260a1
karthikq <kk968346@gmail.com>
2022-04-19 11:58:37 +0530

    resp22

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -5,8 +5,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 // Update below to match your own MongoDB connection string.
-const MONGO_URL =
-  "mongodb+srv://admin2:admin1234@cluster0.44gx5.mongodb.net/nasa";
+const MONGO_URL = "";
 
 mongoose.connection.once("open", () => {
   console.log("MongoDB connection ready!");
